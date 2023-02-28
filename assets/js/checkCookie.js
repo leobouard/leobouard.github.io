1dfd636ed7a375b6f03c66c1db9c0d39077bf2ee
Léo Bouard <leo.bouard@hotmail.com>
2023-02-28 10:16:43 +0100

    Modification du sens des cookies

--- assets/js/checkCookie.js
+++ assets/js/checkCookie.js
@@ -1,8 +1,6 @@
 function checkCookie() {
-    if (document.cookie.indexOf("cookie=true") !== -1) {
+    if (document.cookie.indexOf("cookie=true") == -1) {
       var cookieInfo = document.getElementById("cookieInfo");
-      cookieInfo.style.display = "none";
+      cookieInfo.style.display = "flex";
     }
 };
-
-window.onload = checkCookie();
