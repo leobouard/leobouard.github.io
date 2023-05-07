39b2bbd501b78ae410d05fa73469248042630d14
Léo Bouard <leo.bouard@hotmail.com>
2023-05-07 19:14:27 +0200

    Ajout FontAwesome + suppression ancien JS

--- assets/js/checkCookie.js
+++
@@ -1,6 +0,0 @@
-function checkCookie() {
-    if (document.cookie.indexOf("cookie=true") == -1) {
-      var cookieInfo = document.getElementById("cookieInfo");
-      cookieInfo.style.display = "flex";
-    }
-};
--- assets/js/setCookie.js
+++
@@ -1,5 +0,0 @@
-function setCookie() {
-    document.cookie = "cookie=true;max-age=86400";
-    var cookieInfo = document.getElementById("cookieInfo");
-    cookieInfo.style.display = "none";
-}
